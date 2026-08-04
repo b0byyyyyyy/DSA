114f8d95202587aaed27ae5daafc3ceb48972d7f
b0byyyyyyy <bobybro500@gmail.com>
2026-08-04 21:58:56 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (58.87%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -4,8 +4,8 @@ public:
         if (n == 1) return 1;
         if (n == 2) return 2;
 
-        int prev2 = 1; // Ways to reach step 1
-        int prev1 = 2; // Ways to reach step 2
+        int prev2 = 1; 
+        int prev1 = 2;
         int curr = 0;
 
         for (int i = 3; i <= n; i++) {
