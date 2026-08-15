d9ff459f503bf4be9c30201191d0bbbe6f125630
b0byyyyyyy <bobybro500@gmail.com>
2026-08-15 10:30:49 +0530

    Time: 17 ms (45.20%) | Memory: 16.6 MB (28.05%) - LeetSync

---
+++ 2887-sort-vowels-in-a-string/sort-vowels-in-a-string.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    string sortVowels(string s) {
+        vector<int>lower(26,0);
+        vector<int>upper(26,0);
+
+        for(int i=0; i<s.size(); i++){
+            //lower casewala
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o'|| s[i]=='u'){
+                lower[s[i]-'a']++;
+                s[i]='#';
+
+            }
+
+            //upper casewala
+             if(s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O'|| s[i]=='U'){
+                upper[s[i]-'A']++;
+                s[i]='#';
+
+            }
+    }
+
+    string vowel;
+    //pehel upper wala ayega
+      for(int i=0; i<26; i++){
+        char c='A'+i;
+        while(upper[i]){
+            vowel+=c;
+            upper[i]--;
+        }
+      }
+      //ab lower wala
+
+      for(int i=0; i<26; i++){
+        char c='a'+i;
+        while(lower[i]){
+            vowel+=c;
+            lower[i]--;
+        }
+      }
+      int first=0 , second =0;
+
+      while(second<vowel.size()){
+        if(s[first]=='#'){
+            s[first]=vowel[second];
+            second++;
+        }
+        first++;
+      
+      }
+      return s;
+    }
+
+    };
