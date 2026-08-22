b78ac9b122ab3237b20de3e0e021832b2490a020
b0byyyyyyy <bobybro500@gmail.com>
2026-08-22 17:13:40 +0530

    Time: 3 ms (16.66%) | Memory: 7.8 MB (74.35%) - LeetSync

---
+++ 374-guess-number-higher-or-lower/guess-number-higher-or-lower.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int guessNumber(int n) {
+        int low=1,high=n;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(guess(mid)==-1){
+                high=mid-1;
+            }
+            else if(guess(mid)==1){
+                  low=mid+1;
+            }
+            else{
+                   return mid;
+            }
+        }
+        return 0;
+    }
+};
